80b833677dafc64a11ba42197339a0d466ad4d32
Mehedi <mehedihasan712277@gmail.com>
2026-07-11 01:08:31 +0600

    project initiated with express

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import cookieParser from "cookie-parser";
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
+import config from "./config";
+
+// import { notFound } from "./middleware/notfound";
+// import { globalErrorHandler } from "./middleware/globalErrorHandler";
+
+const app: Application = express();
+
+app.use(
+    cors({
+        origin: config.app_url,
+        credentials: true,
+    }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello world");
+});
+
+// app.use(notFound);
+
+// app.use(globalErrorHandler);
+
+export default app;
