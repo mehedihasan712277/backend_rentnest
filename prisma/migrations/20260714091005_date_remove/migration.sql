fc654bd13fb99488e8c0fac38e240f717af6916d
Mehedi <mehedihasan712277@gmail.com>
2026-07-14 15:10:44 +0600

    refact: change rentarequest model

---
+++ prisma/migrations/20260714091005_date_remove/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `moveInDate` on the `rentalrequests` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "rentalrequests" DROP COLUMN "moveInDate";
